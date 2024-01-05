1c15f6732b37b353256b91055d91f0e5aea7c70f
김다슬 <daseul727@hankookn.com>
2024-01-05 16:24:46 +0900

    _app, _document 변경

--- react_next/pages/_app.tsx
+++ react_next/pages/_app.tsx
@@ -12,16 +12,16 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+    /*
+    서버로 요청이 들어왔을 때 가장 먼저 실행되는 컴포넌트, 페이지에 적용할 공통 레이아웃을 만듦
+    localhost/home 접속 시, Component 인자값으로 home 컴포넌트가 들어오게 된다
+
+    로직, 전역 스타일 등 컴포넌트에 공통 데이터를 다룬다
+    */
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return(
       <>
-        <Head>
-          <title>사이트명 : 타이틀입니다</title>
-          <meta name='viewport' content='initial-scale=1, maximum-scale=1, width=device-width' />
-          <meta property='og:type' content='website' />
-          <meta name='description' content='사이트 설명입니다' />
-        </Head>
         {getLayout(<Component {...pageProps} />)}
       </>
   )
--- react_next/pages/_document.tsx
+++ react_next/pages/_document.tsx
@@ -1,9 +1,25 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+
+    /*
+    _app 다음 실행되며 공통적으로 사용할 head, 메타태그, body 를 커스텀
+
+    주요 사용목적
+    1. import font
+    2. char-set, 웹 접근성 관련 태그 설정
+
+    공통적으로 적용할 HTML 마크업을 중심으로 다룬다
+    */
+
   return (
     <Html lang="en">
-      <Head />
+        <Head>
+            <title>사이트명 : 타이틀입니다</title>
+            <meta name='viewport' content='initial-scale=1, maximum-scale=1, width=device-width' />
+            <meta property='og:type' content='website' />
+            <meta name='description' content='사이트 설명입니다' />
+        </Head>
       <body>
         <Main />
         <NextScript />
